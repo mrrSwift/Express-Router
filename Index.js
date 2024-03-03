bca437e20e7cd35b59e7639ad5fce795db857791
Mr Swift <amirshayan1381@yahoo.com>
2024-03-04 03:14:29 +0330

    add

---
+++ Index.js
@@ -0,0 +1,22 @@
+const { readdirSync } = require('fs');
+const path = require('path');
+const swift = require('swiftly-tools');
+
+module.exports.fetchRoute = (app, routesAddress = "./routes") => {
+
+    for (const file of readdirSync(routesAddress)) {
+        try {
+            const routeFile = require(routesAddress + "/" + file)
+            app.use(routeFile.route, routeFile.router)
+            swift.color.console(routeFile.route + " Added as route", 'fgGreen')
+        } catch (error) {
+            swift.color.console("Can't add " + file, 'fgRed')
+        }
+    }
+
+
+}
+
+module.exports.autoFetch = (router, controllersAddress) => {
+
+}
