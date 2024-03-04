9146f856bd879839e1e8570f31adac4c9ff12119
Mr Swift <amirshayan1381@yahoo.com>
2024-03-04 03:59:30 +0330

    Update Index.js

--- Index.js
+++ Index.js
@@ -8,15 +8,42 @@ module.exports.fetchRoute = (app, routesAddress = "./routes") => {
         try {
             const routeFile = require(routesAddress + "/" + file)
             app.use(routeFile.route, routeFile.router)
-            swift.color.console(routeFile.route + " Added as route", 'fgGreen')
+            swift.color.console(routeFile.route + " route loaded", 'fgGreen')
         } catch (error) {
-            swift.color.console("Can't add " + file, 'fgRed')
+            swift.color.console("Can't load " + file, 'fgRed')
         }
     }
 
 
 }
 
-module.exports.autoFetch = (app, router, routesAddress = "./routes", controllersAddress = "./routes", middlewareAddress = "./routes") => {
+module.exports.autoFetch = (app, router, controllersAddress = "./controllers", middlewareAddress = "./middleware") => {
+
+    const middelware = {}
+
+    for (const file of readdirSync(middlewareAddress)) {
+        try {
+            const middelwareFile = require(middlewareAddress + "/" + file)
+            middelware[middelwareFile.name] = middelwareFile.run
+            swift.color.console(middelwareFile.name + " middleware loaded", 'fgGreen')
+        } catch (error) {
+            swift.color.console("Can't load " + file, 'fgRed')
+
+        }
+    }
+
+    for(const file of readdirSync(controllersAddress)){
+
+        const controllerFile =  require(controllersAddress + "/" + file)
+        const usage = []
+        controllerFile.middelware.forEach(item => {
+            usage.push(middelware[item])
+        });
+        usage.push(controllerFile.run)
+        router.use(controllerFile.method, usage)
+
+    }
+
+
 
 }
