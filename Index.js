e0ae9a3b55db48c45083ae54350751dc0457e75f
Mr Swift <amirshayan1381@yahoo.com>
2024-05-05 19:46:01 +0330

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,80 +1,67 @@
 const { readdirSync, mkdir } = require('fs');
-let routeCount = 0
+const { constants } = require("../constants");
 const readline = require("readline");
-let rl = readline.createInterface(process.stdin, process.stdout);
 const os = require('os');
 const path = require('path');
 let fetcher = false
-const { constants } = require("../constants");
+let rl = readline.createInterface(process.stdin, process.stdout);
+let routeCount = 0
 
-module.exports.errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500;
-  
-    switch (statusCode) {
-      case constants.VALIDATION_ERROR:
-        res.status(statusCode).json({
-          title: "Validation Failed ",
-          msg: err.message,
-          stackTrace: err.stack,
-        });
-        break;
-      case constants.NOT_FOUND:
-        res.status(statusCode).json({
-          title: "Not found ",
-          msg: err.message,
-          stackTrace: err.stack,
-        });
-        break;
-      case constants.FORBIDOEN:
-        res.status(statusCode).json({
-          title: "Forbidoen ",
-          msg: err.message,
-          stackTrace: err.stack,
-        });
-        break;
-      case constants.SERVER_ERROR:
-        res.status(statusCode).json({
-          title: "Server Error ",
-          msg: err.message,
-          stackTrace: err.stack,
-        });
-        break;
-      case constants.UNAUTHORIZED:
-        res.status(statusCode).json({
-          title: "Unauthorized ",
-          msg: err.message,
-          stackTrace: err.stack,
-        });
-        break;
-      case constants.SITE_UNAUTHORIZED:
-        res.status(statusCode).json({
-          title: "Site Unauthorized ",
-          msg: err.message,
-          stackTrace: err.stack,
-        });
-        break;
-      case constants.TOO_MANY:
-        res.status(statusCode).json({
-          title: "Too many request",
-          msg: err.message,
-          stackTrace: err.stack,
-        });
-        break;
-      default:
-        console.log(err);
-        res.status(500).json({ err: err.message, msg: "Unhandled error" })
-        break;
-    }
-  };
+module.exports.errorHandler = (trace)=>{
+    return (err, req, res, next) => {
+        const statusCode = res.statusCode ? res.statusCode : 500;
+        const errMsg = {
+            title: "",
+            msg: err.message,
+        }
+        if(trace){
+            errMsg.stackTrace = err.stack
+        }
+        switch (statusCode) {
+            case constants.VALIDATION_ERROR:
+                errMsg.title = "Validation Failed "
+                res.status(statusCode).json(errMsg);
+                break;
+            case constants.NOT_FOUND:
+                errMsg.title = "Not found "
+                res.status(statusCode).json(errMsg);
+                break;
+            case constants.FORBIDOEN:
+                errMsg.title = "Forbidoen "
+                res.status(statusCode).json(errMsg);
+                break;
+            case constants.SERVER_ERROR:
+                errMsg.title = "Server Error "
+                res.status(statusCode).json(errMsg);
+                break;
+            case constants.UNAUTHORIZED:
+                errMsg.title = "Unauthorized "
+                res.status(statusCode).json(errMsg);
+                break;
+            case constants.SITE_UNAUTHORIZED:
+                errMsg.title = "Site Unauthorized "
+                res.status(statusCode).json(errMsg);
+                break;
+            case constants.TOO_MANY:
+                errMsg.title = "Too many request "
+                res.status(statusCode).json(errMsg);
+                break;
+            default:
+                console.log(err);
+                res.status(500).json({ err: err.message, msg: "Unhandled error" })
+                break;
+        }
+    };
+}
 
-module.exports.cli = (express)=>{
-    if(!fetcher){
+module.exports.cli = (express) => {
+    if (!fetcher) {
         console.log(colorful(`┳┳┓    ┏┓   •┏ `, 'bgCyan'))
         console.log(colorful(`┃┃┃┏┓  ┗┓┓┏┏┓╋╋`, 'bgCyan'))
         console.log(colorful(`┛ ┗┛   ┗┛┗┻┛┗┛┗\n`, 'bgCyan'))
     }
     fetcher = true
-    rl.on('line',async (input) => {
+    rl.on('line', async (input) => {
         switch (input) {
             case "routes":
                 console.log(colorful(routeCount + " Route loaded", 'fgRed'))
@@ -95,45 +82,45 @@ module.exports.cli = (express)=>{
             case "env":
                 console.log(process.env)
                 break;
-            
+
             case "init":
-               mkdir(path.join(process.cwd(), `/admins/`),()=>{
-                console.log('The *admins* folder has been created')
-               });
-               mkdir(path.join(process.cwd(), `/config/`),()=>{
-                console.log('The *config* folder has been created')
-               });
-               mkdir(path.join(process.cwd(), `/routes/`),()=>{
-                console.log('The *routes* folder has been created')
-               });
-               mkdir(path.join(process.cwd(), `/controllers/`),()=>{
-                console.log('The *controllers* folder has been created')
-               });
-               mkdir(path.join(process.cwd(), `/middleware/`),()=>{
-                console.log('The *middleware* folder has been created')
-               });
-               mkdir(path.join(process.cwd(), `/plugins/`),()=>{
-                console.log('The *plugins* folder has been created')
-               });
-               mkdir(path.join(process.cwd(), `/uploads/`),()=>{
-                console.log('The *uploads* folder has been created')
-               });
-               mkdir(path.join(process.cwd(), `/models/`),()=>{
-                console.log('The *models* folder has been created\n')
-               });
+                mkdir(path.join(process.cwd(), `/admins/`), () => {
+                    console.log('The *admins* folder has been created')
+                });
+                mkdir(path.join(process.cwd(), `/config/`), () => {
+                    console.log('The *config* folder has been created')
+                });
+                mkdir(path.join(process.cwd(), `/routes/`), () => {
+                    console.log('The *routes* folder has been created')
+                });
+                mkdir(path.join(process.cwd(), `/controllers/`), () => {
+                    console.log('The *controllers* folder has been created')
+                });
+                mkdir(path.join(process.cwd(), `/middleware/`), () => {
+                    console.log('The *middleware* folder has been created')
+                });
+                mkdir(path.join(process.cwd(), `/plugins/`), () => {
+                    console.log('The *plugins* folder has been created')
+                });
+                mkdir(path.join(process.cwd(), `/uploads/`), () => {
+                    console.log('The *uploads* folder has been created')
+                });
+                mkdir(path.join(process.cwd(), `/models/`), () => {
+                    console.log('The *models* folder has been created\n')
+                });
                 break;
 
             default:
                 console.log("Commend not found!")
                 break;
         }
-        
+
     })
 }
 
-module.exports.autoFetch = (express,pAddress = "hasPermission", cAddress = "controllers", mAddress = "middleware") => {
+module.exports.autoFetch = (express, pAddress = "hasPermission", cAddress = "controllers", mAddress = "middleware") => {
 
-    if(!fetcher){
+    if (!fetcher) {
         console.log(colorful(`┳┳┓    ┏┓   •┏ `, 'bgCyan'))
         console.log(colorful(`┃┃┃┏┓  ┗┓┓┏┏┓╋╋`, 'bgCyan'))
         console.log(colorful(`┛ ┗┛   ┗┛┗┻┛┗┛┗\n`, 'bgCyan'))
@@ -141,13 +128,13 @@ module.exports.autoFetch = (express,pAddress = "hasPermission", cAddress = "cont
 
     fetcher = true
     const middleware = {}
-    const controllersAddress = path.join("./",cAddress)
-    const middlewareAddress = path.join("./",mAddress)
+    const controllersAddress = path.join("./", cAddress)
+    const middlewareAddress = path.join("./", mAddress)
 
     try {
         for (const file of readdirSync(middlewareAddress)) {
             try {
-                const folderPath = path.join(__dirname,"../../",middlewareAddress, file)
+                const folderPath = path.join(__dirname, "../../", middlewareAddress, file)
                 const middlewareFile = require(folderPath)
                 middleware[middlewareFile.name] = middlewareFile.run
                 console.log(colorful("🟢 ~ " + middlewareFile.name + " middleware loaded ", 'fgGreen'))
@@ -164,7 +151,7 @@ module.exports.autoFetch = (express,pAddress = "hasPermission", cAddress = "cont
 
     const baseRouter = express.Router();
     for (const controllerFile of readdirSync(controllersAddress)) {
-        const folderPath = path.join(__dirname,"../../",controllersAddress, controllerFile)
+        const folderPath = path.join(__dirname, "../../", controllersAddress, controllerFile)
         const controller = require(folderPath)
         const router = express.Router();
         controller.items.forEach(item => {
@@ -174,10 +161,10 @@ module.exports.autoFetch = (express,pAddress = "hasPermission", cAddress = "cont
                 const usage = []
                 if (item?.middleware) {
                     item.middleware.forEach(midd => {
-                        if(midd == pAddress){
+                        if (midd == pAddress) {
                             usage.push(middleware[midd](item.permission))
 
-                        }else{
+                        } else {
                             usage.push(middleware[midd])
                         }
                     })
@@ -219,8 +206,8 @@ const colorCons = {
     bgMagenta: "\x1b[45m",
     bgCyan: "\x1b[46m",
     bgWhite: "\x1b[47m"
-  }
-const colorful = (text, color)=> {
+}
+const colorful = (text, color) => {
     return colorCons[color] + text + colorCons.reset
 }
 
